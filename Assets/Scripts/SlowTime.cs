9d729fc4371cfe1c8fd6c7350cb50fda517222dd
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2021-02-23 16:04:08 -0700

    it kinda work

--- Assets/Scripts/SlowTime.cs
+++ Assets/Scripts/SlowTime.cs
@@ -8,11 +8,6 @@ public class SlowTime : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //first we make sure that the object that hit the banana is the player.
-        //if (other.name == "Player")
-        //{
-            // This will search all player scripts for a function called "Hit Banana"
-            Time.timeScale = time; 
-        //}
+        Time.timeScale = time;
     }
 }
