c87a6c55dc6d25de92c378bdb8ea8ba5771db274
inexperiencedlightning <inexperiencedlightning@users.noreply.github.com>
2021-03-03 21:08:20 -0700

    Changed the following scripts.
    Dragon looks a little janky but it flips now.

--- Assets/Scripts/AIFollow.cs
+++ Assets/Scripts/AIFollow.cs
@@ -6,6 +6,9 @@ public class AIFollow : MonoBehaviour
 {
     public float speed = 3f;
     public float turnSpeed = 3f;
+    public float distance = 25f;
+    public float sprintSpeed = 6f;
+    public float size = 1f;
     private Transform target;
 
     void Start()
@@ -20,10 +23,22 @@ public class AIFollow : MonoBehaviour
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
 
-        float dist = Vector2.Distance(target.transform.position, transform.position);
-        if (dist > 0)
+        if(direction.x > 0)
         {
+            transform.localScale = new Vector3(size, size, size);
+        }
+        else if(direction.x < 0)
+        {
+            transform.localScale = new Vector3(size, -size, size);
+        }
+
+        float dist = Vector2.Distance(target.transform.position, transform.position);
+        if (dist > 0 && dist < distance){
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+           
+        }
+        else if(dist > distance){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, sprintSpeed * Time.deltaTime);
         }
 
     }
--- Assets/Scripts/bodyFollow.cs
+++ Assets/Scripts/bodyFollow.cs
@@ -6,34 +6,43 @@ public class bodyFollow : MonoBehaviour
 {
     public float speed = 3f;
     public float turnSpeed = 3f;
-    public float distance;
-    public GameObject target;
-    private SpriteRenderer mySpriteRenderer;
+    public float distance = 25f;
+    public float sprintSpeed = 6f;
+    public float size = 1f;
+    public GameObject thingToFollow;
+    private Transform target;
 
-    // This function is called just one time by Unity the moment the component loads
-    private void Awake()
+    void Start()
     {
-        // get a reference to the SpriteRenderer component on this gameObject
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        target = thingToFollow.GetComponent<Transform>();
     }
 
     void Update()
     {
-
         Vector2 direction = target.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
 
-        
-        float dist = Vector2.Distance(target.transform.position, transform.position);
-        if (dist > distance + 1)
+        if (direction.x > 0)
         {
-
-                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-            
+            transform.localScale = new Vector3(size, size, size);
+        }
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(size, -size, size);
         }
 
-      
+        float dist = Vector2.Distance(target.transform.position, transform.position);
+        if (dist > 1 && dist < distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        }
+        else if (dist > distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, sprintSpeed * Time.deltaTime);
+        }
+
     }
 }
